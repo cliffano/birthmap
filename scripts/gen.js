515ceccd93b1459e4cc5fb2dcac6c784a6e3bd11
Cliffano Subagio <cliffano@gmail.com>
2016-04-20 23:10:58 +1000

    Add script to generate geojson file from Wikipedia search list.

---
+++ scripts/gen.js
@@ -0,0 +1,93 @@
+var async       = require('async');
+var fs          = require('fs');
+var geocode     = require('google-geocode');
+var readline    = require('readline');
+var WikiScraper = require('wikiscraper');
+
+geocode.setApiKey(process.env.GOOGLE_DEVELOPER_API_KEY);
+
+// Usage: node scripts/gen.js sports/basketball/usa/dream_team.txt sports/basketball/usa/dream_team.geojson
+var inFile  = process.argv[2];
+var outFile = process.argv[3];
+
+function initFeatureCollection() {
+  return {
+    type: 'FeatureCollection',
+    features: []
+  };
+}
+
+function initFeature(name) {
+  return {
+    type: 'Feature',
+    properties: {
+      name: name,
+      born: ''
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [0, 0]
+    }
+  };
+}
+
+function getFeature(name, cb) {
+  var wikiscraper = new WikiScraper([name]);
+
+  console.log('Scraping Wikipedia page of %s', name);
+  wikiscraper.scrape(function(err, element) {
+    var feature = initFeature(name);
+    if (err) {
+      cb(err);
+    }
+    else {
+      var born_elems = element.infobox.fields.Born.split('\n');
+      var born = born_elems[born_elems.length - 1];
+      feature.properties.born = born;
+
+      console.log('Geocoding %s birthplace in %s', name, born);
+      geocode.getGeocode(born,
+        function (geocode) {
+          var location = JSON.parse(geocode).results[0].geometry.location;
+          feature.geometry.coordinates = [location.lng, location.lat];
+          cb(null, feature);
+        },
+        function (err) {
+          cb(err)
+        });
+    }
+  });
+
+}
+function getFeatures(names, cb) {
+  var tasks = [];
+  names.forEach(function (name) {
+    var task = function (cb) {
+      getFeature(name, cb);
+    };
+    tasks.push(task);
+  });
+  async.parallel(tasks, cb);
+}
+
+var names = [];
+
+console.log('Reading input file %s', inFile);
+var rl = readline.createInterface({
+  input: fs.createReadStream(inFile)
+});
+rl.on('line', function (name) {
+  names.push(name);
+});
+rl.on('close', function () {
+  var data = initFeatureCollection();
+  getFeatures(names, function (err, features) {
+    if (err) {
+      console.error(err);
+    } else {
+      data.features = features;
+      console.log('Writing output file %s', outFile);
+      fs.writeFileSync(outFile, JSON.stringify(data, null, 2));
+    }
+  });
+});
